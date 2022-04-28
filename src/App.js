99caa3a624df4cfcd85767666e0e0b33ac46fc55
Déborah <deborahclerckx@gmail.com>
2022-04-29 00:23:17 +0200

    correctif index.js + mélange des cartes, fonctionnel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState } from 'react'
 import './App.css'
 
+const cardImages = [
+  { "src": "/img/earth.JPG"},
+  { "src": "/img/lightning.JPG"},
+  { "src": "/img/air.JPG"},
+  { "src": "/img/water.JPG"},
+  { "src": "/img/fire.JPG"},
+  { "src": "/img/cover.JPG"}
+]
+
 function App() {
+const [cards, setCards] = useState([])
+const [turns, setTurns] = useState(0)
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New game</button>
+      <button onClick={shuffleCards}>New game</button>
     </div>
   );
 }
